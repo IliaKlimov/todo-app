be630aa9ac43a4c655f8d9494816d78932461f3d
Pelerin <infuzer@gmail.com>
2019-12-12 18:23:31 +0300

    fix

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1_f4v_m)#cq1jkm^uclzio&8im(vgi-y3q0wazha$q*5w4vtmz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.120", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
